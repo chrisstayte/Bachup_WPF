c081d97b2c6f62045fc02f80dfac5990d34640ae
Chris Stayte <ctstayte@gmail.com>
2019-03-18 11:34:47 -0400

    Added Changes

---
+++ Bachup/ViewModel/BachupGroupViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Bachup.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bachup.ViewModel
+{
+    class BachupGroupViewModel : BaseViewModel
+    {
+        public BachupGroupViewModel(BachupGroup BachupGroupInput)
+        {
+            bachupGroup = BachupGroupInput;
+            Name = bachupGroup.Name;
+        }
+
+        private BachupGroup bachupGroup;
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Bachup/ViewModel/HomePageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bachup.ViewModel
+{
+    class HomePageViewModel : BaseViewModel
+    {
+        public HomePageViewModel()
+        {
+            Message = "Thanks For Using Bachup";
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            VersionNumber = String.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+        }
+
+        private string _message;
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _versionNumber;
+        public string VersionNumber
+        {
+            get { return _versionNumber; }
+            set
+            {
+                _versionNumber = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
