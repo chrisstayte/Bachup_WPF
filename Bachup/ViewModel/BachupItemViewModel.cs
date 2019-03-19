466e76ef563490cb6a7c1284bdaf8f3e29370723
Chris Stayte <ctstayte@gmail.com>
2019-03-19 13:10:43 -0400

    I need to go home and sleep

--- Bachup/ViewModel/BachupItemViewModel.cs
+++ Bachup/ViewModel/BachupItemViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Bachup.Model;
+﻿using Bachup.Helpers;
+using Bachup.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Bachup.ViewModel
 {
@@ -12,8 +15,13 @@ namespace Bachup.ViewModel
 
         public BachupItemViewModel(BachupItem item)
         {
+            // Connect Commands On Creations
+            BachupBachupItemCommand = new RelayCommand(BachupBachupItem);
 
             BachupItem = item;
+
+            
+
         }
 
         private BachupItem _bachupItem;
@@ -35,5 +43,83 @@ namespace Bachup.ViewModel
             }
         }
 
+        private bool _isSaving;
+        public bool IsSaving
+        {
+            get { return _isSaving; }
+            set
+            {
+                _isSaving = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double _saveProgress;
+        public double SaveProgress
+        {
+            get { return _saveProgress; }
+            set
+            {
+                _saveProgress = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isSaveComplete;
+        public bool IsSaveComplete
+        {
+            get { return _isSaveComplete; }
+            set
+            {
+                _isSaveComplete = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // Relay Commands
+
+        public RelayCommand BachupBachupItemCommand { get; private set; }
+
+
+
+        #region Events
+
+        private void BachupBachupItem(object parameter)
+        {
+            if (IsSaveComplete == true)
+            {
+                IsSaveComplete = false;
+                return;
+            }
+
+            if (SaveProgress != 0) return;
+
+            var started = DateTime.Now;
+            IsSaving = true;
+
+            new DispatcherTimer(
+                   TimeSpan.FromMilliseconds(50),
+                   DispatcherPriority.Normal,
+                   new EventHandler((o, e) =>
+                   {
+                       var totalDuration = started.AddSeconds(3).Ticks - started.Ticks;
+                       var currentProgress = DateTime.Now.Ticks - started.Ticks;
+                       var currentProgressPercent = 100.0 / totalDuration * currentProgress;
+
+                       SaveProgress = currentProgressPercent;
+
+                       if (SaveProgress >= 100)
+                       {
+                           IsSaveComplete = true;
+                           IsSaving = false;
+                           SaveProgress = 0;
+                           ((DispatcherTimer)o).Stop();
+                       }
+
+                   }), Dispatcher.CurrentDispatcher);
+        }
+
+
+        #endregion
     }
 }
